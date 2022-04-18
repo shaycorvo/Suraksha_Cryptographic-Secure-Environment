4c97f59b49bb573314157bcfa49d0818e1f00caa
shaycorvo <shaycorvo@gmail.com>
2022-04-18 23:55:25 +0530

    Phase 3 Strt

--- Encriptor.java
+++ Encriptor.java
@@ -4,14 +4,30 @@ import java.util.*;
 public class Encriptor
 {
 
-	 private char key;
-	  private String substitutionKey;
-	  private Hashtable codeTable  = new Hashtable(26);
-	  private Hashtable decodeTable = new Hashtable(26);
+  private char key;
+  private String substitutionKey;
+  private Hashtable codeTable  = new Hashtable(26);
+  private Hashtable decodeTable = new Hashtable(26);
 
- public Encriptor(char c)//constructor
+ public Encriptor(char key)//constructor
  {
- 
+ this.key = key;
+ substitutionKey = null;
+ }
+
+ public Encriptor(String key)// overloaded Constructor
+ {
+	 if(key.length() > 26)
+	  throw new IllegalArgumentException("the key must be 26 alphabets");
+	 if(hasRepeatedCharacters(key))
+	  throw new IllegalArgumentException("the key must not have repeated alphabets");
+
+	 this.substitutionKey = key;
+	 for(int i=0;i<key.length();i++)
+	  {
+		  codeTable.put(new Character((char)(65 + i)),new Character(Character.toUpperCase(substitutionKey.charAt(i))));
+		  decodeTable.put(new Character(Character.toUpperCase(substitutionKey.charAt(i))),new Character((char)(65 + i)));
+	  }
  }
 
  public void setCharKey(char key)
@@ -98,38 +114,81 @@ public String decodeCaeser(String s)
 
 private char codeCharacter(char ch,char offset)
  {
-	return offset;
-	  
+	  char upperA = 'A';
+	  char upperZ = 'Z';
+	  char length = 26;
+	  char coded = Character.toUpperCase(ch);
+	  coded += offset;
+	  if(coded > upperZ)
+	  {
+		  coded = (char) (coded - upperZ);
+		  coded = (char) ((upperA + coded)-1);
+	  }
+	  if(Character.isUpperCase(ch))
+	    return coded;
+	  else
+	    return Character.toLowerCase(coded);
 }
 
 private char deCodeCharacter(char ch,char offset)
 {
-	return offset;
-	
+	char upperA = 'A';
+	char coded = Character.toUpperCase(ch);
+	  coded -= offset;
+
+	if(coded < upperA)
+	 coded =(char)(coded + 26);
+
+	return (Character.isUpperCase(ch))?coded:Character.toLowerCase(coded);
 }
 
 public String codeSubstitutionCipher(String s)
 {
-	return s;
-  
+  char[] cArray = s.toCharArray();
+  String coded;
+  for(int i =0;i<s.length();i++)
+  {
+	  if(Character.isUpperCase(s.charAt(i))||Character.isLowerCase(s.charAt(i)))
+	  {
+		  cArray[i] = codeSubstitutionCharacter(cArray[i]);
+	  }
+  }
+  return new String(cArray);
 }
 
 public String decodeSubstitutionCipher(String s)
 {
-	return s;
-  
+  char[] cArray = s.toCharArray();
+
+  for(int i =0;i<s.length();i++)
+  {
+	  if(Character.isUpperCase(s.charAt(i))||Character.isLowerCase(s.charAt(i)))
+	  {
+		  cArray[i] = decodeSubstitutionCharacter(cArray[i]);
+	  }
+  }
+  return new String(cArray);
 }
 
 private char codeSubstitutionCharacter(char ch)
 {
-	return ch;
-	
+	Character key = new Character(Character.toUpperCase(ch));
+	Character character = (Character) (codeTable.get(key));
+	if(Character.isUpperCase(ch))
+	   return Character.toUpperCase(character.charValue());
+	else
+	   return Character.toLowerCase(character.charValue());
 }
 
 private char decodeSubstitutionCharacter(char ch)
 {
-	return ch;
-	
+	Object obj = decodeTable.get(new Character(Character.toUpperCase(ch)));
+	Character character = (Character) obj;
+	if(Character.isUpperCase(ch))
+	 return Character.toUpperCase(character.charValue());
+    else
+	 return Character.toLowerCase(character.charValue());
+	 //return character.charValue();
 }
 
 
@@ -151,6 +210,42 @@ public boolean hasRepeatedCharacters(String key)
  }
 }// end class
 
+/*
+public static void main(String args[])throws FileNotFoundException,IOException
+{
+	Encriptor enc = new Encriptor('F');
+	String text = "ABCDEFGHIJK LMNO ghghghg 5YZabcde ABC";
+	System.out.println(text);
+	String codedString = enc.codeCaeser(text);
+	System.out.println(codedString);
+	System.out.println(enc.decodeCaeser(codedString));
+	//FileOutputStream fos = new FileOutputStream("code.dat");
+	File f = new File("coded.txt");
+	f.createNewFile();
+	enc.writeBytes(enc.codeCaeser(text),f);
+	System.out.println(f.length());
+	String s = enc.readBytes(f);
+
+	System.out.println(s);
+	System.out.println("\n\n\n\n" + enc.decodeCaeser(s));
+
+	System.out.println("Now decoding by substitution cypher\n\n\n");
+	String key = "abefcdHGJIKLMNOPQRSTUVWxyz";
+	String message  = "KHURAM IS A STRANGE 123 and MAN ZZ";
+	System.out.println("The message is      " + message);
+	System.out.println("The key is " + key);
+	Encriptor enc2 = new Encriptor(key);
+	String codemessage = enc2.codeSubstitutionCipher(message);
+	System.out.println("The coded message is " + codemessage);
+	String decodedmessage  = enc2.decodeSubstitutionCipher(codemessage);
+	System.out.println("The decoded message is "+ decodedmessage);
+
+
+		// System.out.println(enc2.codeTable);
+		// System.out.println(enc2.decodeTable);
+
+}
+*/
 
 
 
